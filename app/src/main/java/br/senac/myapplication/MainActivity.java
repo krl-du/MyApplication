863e2f589ee7067160670070ac550acd26f57d45
eduardo.psilva18 <eduardo.dup12@gmail.com>
2019-02-11 22:08:28 -0200

    Commit inicial.

--- app/src/main/java/br/senac/myapplication/MainActivity.java
+++ app/src/main/java/br/senac/myapplication/MainActivity.java
@@ -1,11 +1,16 @@
 package br.senac.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         ListView listaContatos = findViewById(R.id.ListaContatos);
 
         //Cria Lista de contTOA COMO STRING
-        String [] contatos = {"André","Ari", "Roberto", "luzia"};
+        String [] contatos = {"Duplo Cilindro", "Mono Cilindro", "Aldo", "Ponga", "recupera filo", "Mancata ucita della calza", "abassa aghi scarico bordo", "Fillo Rotto ", "abassa aghi talone", "Tiragio", "Apripalete scarico bordo", "Filo no tagliato"};
 
         // PARA CONSEGUIRMOS EXIBIR A LISTA DE LISTVIEW PRECISO
         //CRIAR UM ADAPTOR
@@ -24,6 +29,19 @@ public class MainActivity extends AppCompatActivity {
 
         // insere o adaptor na lista de contatos
         listaContatos.setAdapter(adapter);
+
+        //Recuperar o botão e tcriar ação pra ele
+        Button novocontato = findViewById(R.id.NovoContatoButton);
+
+        novocontato.setOnClickListener(new View.OnClickListener() {
+                                          @Override
+                                          public void onClick(View v) {
+                                              Intent contato = new Intent(MainActivity.this, ContatoActivity.class);
+                                              startActivity(contato);
+                                          }
+                                      }
+
+        );
 
 
     }
